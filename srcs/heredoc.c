dd4b7456b833a7d8d662e5bdd0147ab62ce0e537
Hakim Ahmed <hakahmed@c1r13s1.42madrid.com>
2023-04-11 10:01:00 +0200

    finally, the heredoc works (although im not sure why)

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,13 +6,12 @@
 /*   By: hakahmed <hakahmed@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 17:11:04 by hakahmed          #+#    #+#             */
-/*   Updated: 2023/04/11 04:03:01 by hakahmed         ###   ########.fr       */
+/*   Updated: 2023/04/11 04:30:03 by hakahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
-#include <sys/_types/_s_ifmt.h>
 #include <sys/fcntl.h>
 #include <unistd.h>
 
@@ -23,46 +22,29 @@ void	ft_heredoc(char *dl, char **envp, char *cmd, int fd[2])
 	pid_t pid = ft_fork();
 	if (pid)
 	{
-		// ft_printf("heredoc> ");
-		int tmpfd = open("tmp", O_CREAT | O_TRUNC | O_RDWR, S_IRWXU );
-		// close(fd[WRITE_END]);
-		char *line = get_next_line(0);
+		char *line = get_next_line(STDIN_FILENO);
 		while (line)
 		{
-			if (!ft_strncmp(dl, line, ft_strlen(dl) - 1))
+			if (!ft_strncmp(dl, line, ft_strlen(line) - 1))
 				break ;
 			ft_putstr_fd(line, fd[WRITE_END]);
-			// free(line);
-			line = get_next_line(0);
+			line = get_next_line(STDIN_FILENO);
 		}
 		free(line);
-		// dup2(fd[READ_END], STDIN_FILENO);
-		close(tmpfd);
-		tmpfd = open("tmp", O_RDONLY);
-		close(tmpfd);
+		close(fd[WRITE_END]);
 		dup2(fd[READ_END], STDIN_FILENO);
 		close(fd[READ_END]);
-		close(fd[WRITE_END]);
-		waitpid(pid, &status, 0);
-		// exit(0);
-		// ft_execute("cat", envp);
+		waitpid(pid, &status, -1);
 	}
 	else
 	{
-		// waitpid(pid, &status, 0);
 		close(fd[READ_END]);
 		dup2(fd[WRITE_END], STDOUT_FILENO);
 		close(fd[WRITE_END]);
-		// int pid2 = ft_fork();
-		// if (!pid2)
-		// {
-		// 	char **rm = ft_split("rm -f tmp", 32);
-		// 	execve(rm[0], rm, envp);
-		// }
-		// waitpid(pid2, NULL, -1);
 		int tmpfd = open("tmp", O_RDONLY);
 		dup2(tmpfd, STDIN_FILENO);
 		ft_execute(cmd, envp);
+		exit(0);
 	}
 }
 
